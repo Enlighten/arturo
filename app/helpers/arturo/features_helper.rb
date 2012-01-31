95df94c4adc1b83d55841445313842d21c35fb28
Eric Cranston <ecranston@enphaseenergy.com>
2012-01-31 11:49:28 -0800

    Removed unnecessary error_messages_for helper. It was conflicting with the dynamic_form error_messages_for helper

--- app/helpers/arturo/features_helper.rb
+++ app/helpers/arturo/features_helper.rb
@@ -23,15 +23,5 @@ module Arturo
     def deployment_percentage_output_tag(id, value)
       content_tag(:output, value, { 'for' => id, 'class' => 'deployment_percentage no_js' })
     end
-
-    def error_messages_for(feature, attribute)
-      if feature.errors[attribute].any?
-        content_tag(:ul, :class => 'errors') do
-          feature.errors[attribute].map { |msg| content_tag(:li, msg, :class => 'error') }.join('').html_safe
-        end
-      else
-        ''
-      end
-    end
   end
 end
